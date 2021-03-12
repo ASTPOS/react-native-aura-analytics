876786544a9b5aeade823d9227a1d69d7b649360
ASTPOS <naveen.astpos@gmail.com>
2021-03-12 14:40:26 -0800

    initla commit

--- App.js
+++ App.js
@@ -1,14 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { WebView } from 'react-native-webview';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    let [order, setOrder] = React.useState(false);
+    return <WebView source={{ uri: 'https://astposaura.com/AdminLTE/login.php' }} />
 }
 
 const styles = StyleSheet.create({
